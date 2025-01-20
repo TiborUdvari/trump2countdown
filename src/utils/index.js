ac9383cd8a2da9eafe02b56f7d0db2667b4dfadb
Tibor Udvari <tibor.udvari@icloud.com>
2025-01-20 23:06:23 +0100

    feat: minimal version

---
+++ src/utils/index.js
@@ -0,0 +1,29 @@
+import {DateTime}  from "luxon";
+
+export function getInaugurationCountdown() {
+const now = DateTime.now();
+  
+// Set to January 20th, 2029 at 12:00 PM Eastern Time
+const inauguration = DateTime.fromObject(
+  { 
+    year: 2029, 
+    month: 1, 
+    day: 20, 
+    hour: 12, 
+    minute: 0, 
+    second: 0 
+  }, 
+  { 
+    zone: "America/New_York" 
+  }
+  );
+
+  const diff = inauguration.diff(now, ['years', 'months', 'days', 'hours', 'minutes', 'seconds']);
+
+  return `${Math.floor(diff.years)} years
+${Math.floor(diff.months)} months
+${Math.floor(diff.days)} days
+${Math.floor(diff.hours)} hours
+${Math.floor(diff.minutes)} minutes
+${Math.floor(diff.seconds)} seconds`;
+}
